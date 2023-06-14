af8472d438ab72a1e1616e86cd2b91e43f3afb00
Ruby-8 <liang.ziy@northeastern.edu>
2023-06-13 22:33:40 -0700

    Update search page layout.(zi)

--- app/src/main/java/edu/northeastern/movieapi/MainActivity.java
+++ app/src/main/java/edu/northeastern/movieapi/MainActivity.java
@@ -2,10 +2,15 @@ package edu.northeastern.movieapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void openSearchActivity(View view){
+        Intent intent = new Intent(this,SearchActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
