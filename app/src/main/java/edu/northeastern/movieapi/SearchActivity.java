370c09ed1e51400bc06f526eabc530723ea8b242
Ruby-8 <liang.ziy@northeastern.edu>
2023-06-14 23:32:37 -0700

    Update search page filters and cancelEdit method.(zi)

--- app/src/main/java/edu/northeastern/movieapi/SearchActivity.java
+++ app/src/main/java/edu/northeastern/movieapi/SearchActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,11 +19,15 @@ import edu.northeastern.movieapi.network.MovieWebService;
 import edu.northeastern.movieapi.network.NetworkThread;
 
 public class SearchActivity extends AppCompatActivity {
+    EditText searchEditText;
+    public void cancelEdit(View view){
+        searchEditText.setText("");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        EditText searchEditText = findViewById(R.id.searchEditText);
+        searchEditText= findViewById(R.id.searchEditText);
         Button seeResultButton = findViewById(R.id.seeResultButton);
         seeResultButton.setOnClickListener(new View.OnClickListener() {
             @Override
