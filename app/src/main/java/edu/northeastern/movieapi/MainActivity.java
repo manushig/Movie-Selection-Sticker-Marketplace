530c2d5277cb96a5d16d0487a86ae0dd0522b9a7
komalup1991 <upadhyay.ko@northeastern.edu>
2023-06-17 15:51:30 -0700

    resource cleanup main page edit

--- app/src/main/java/edu/northeastern/movieapi/MainActivity.java
+++ app/src/main/java/edu/northeastern/movieapi/MainActivity.java
@@ -3,8 +3,11 @@ package edu.northeastern.movieapi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +15,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
-
+  ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView2);
+        Glide.with(this).load(R.drawable.giphy).into(imageView);
     }
 }
