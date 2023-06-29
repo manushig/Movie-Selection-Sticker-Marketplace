f4c53463c9726674608966e830a678063be3cd71
komalup1991 <upadhyay.ko@northeastern.edu>
2023-06-28 19:58:41 -0700

    Add bottom tabs

--- app/src/main/java/edu/northeastern/stickers/LoginActivity.java
+++ app/src/main/java/edu/northeastern/stickers/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import edu.northeastern.movieapi.R;
@@ -12,6 +13,7 @@ import edu.northeastern.movieapi.R;
 public class LoginActivity extends AppCompatActivity {
 
     TextView registerTextView;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         registerTextView = findViewById(R.id.registerTextView);
+        loginButton = findViewById(R.id.loginButton);
 
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, StickerHomeActivity.class));
             }
         });
     }
