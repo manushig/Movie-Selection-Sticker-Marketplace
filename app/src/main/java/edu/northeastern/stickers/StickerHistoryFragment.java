a28de11a44e79433973e2147a3abd448da8a2735
Ruby-8 <liang.ziy@northeastern.edu>
2023-06-29 19:58:09 -0700

    Update the display stickers fragment.

--- app/src/main/java/edu/northeastern/stickers/StickerHistoryFragment.java
+++ app/src/main/java/edu/northeastern/stickers/StickerHistoryFragment.java
@@ -1,23 +1,62 @@
 package edu.northeastern.stickers;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.northeastern.movieapi.R;
+import edu.northeastern.stickers.adapters.UserStickerActivityAdapter;
 
 public class StickerHistoryFragment extends Fragment {
+    private RecyclerView recyclerDisplay;
+    private UserStickerActivityAdapter adapter;
+    private List<UserStickerActivity> usersStickerActivityList;
+
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_stickers);
+
+        recyclerDisplay = findViewById(R.id.recyclerOfDisplay);
+
+        recyclerDisplay.setLayoutManager(new LinearLayoutManager(this));
+        usersStickerActivityList = new ArrayList<UserStickerActivity>();
+
+        createListData();
+
+        adapter = new UserStickerActivityAdapter(this, usersStickerActivityList);
+        recyclerDisplay.setAdapter(adapter);
+        recyclerDisplay.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+
+
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    private void createListData(){
+//        usersStickerActivityList = new ArrayList<>();
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+//                    UserStickerActivity newUserActivity = new UserStickerActivity(snapshot.getValue(),);
+//                    usersStickerActivityList.add(newUserActivity);
+//                }
+//                adapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//
+//        });
     }
 }
